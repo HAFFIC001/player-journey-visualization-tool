3c965f6930499907f4e4edcd2348f34befe7e79e
Aryan Gupta <ag980455@gmail.com>
2026-08-01 18:37:12 +0530

    Update MapViewport.tsx

--- components/map/MapViewport.tsx
+++ components/map/MapViewport.tsx
@@ -10,19 +10,19 @@ export default function MapViewport({
   children,
 }: Props) {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
+    <div className="absolute inset-0">
       <div
-        className="
-          relative
-          aspect-square
-          h-full
-          max-h-full
-        "
+        className="relative mx-auto h-full"
+        style={{
+          aspectRatio: "1 / 1",
+          maxWidth: "100%",
+        }}
       >
         <img
           src={image}
-          alt="minimap"
-          className="absolute inset-0 h-full w-full object-contain select-none"
+          alt="Minimap"
+          className="absolute inset-0 h-full w-full object-fill select-none"
+          draggable={false}
         />
 
         {children}
